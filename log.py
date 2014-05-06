d5a63dbacbd7ffdad801d6154e16b01576b10dec
root <root@sd-43189.dedibox.fr>
2014-05-06 16:50:32 +0200

    Test compta des impressions

--- log.py
+++ log.py
@@ -5,11 +5,13 @@ import string
 fname ="RDC.csv"
 file = open(fname, "r")
 write = open("test.csv","w")
+nb=0
+couleur=0
 try:
 	reader = csv.reader(file)
 	for row in reader:
 #		print '"'+row[1]+'", "' +row[2]+'", "' +row[5]+'", "' +row[7]+'", "'+row[8] +'"'
-		if (row[5] > "2012-10-29T10:00") and (row[5] < "2013-01-02T10:00"):
+		if (row[5] > "2013-10-20T10:00") and (row[5] < "2013-10-29T10:00"):
 			#	print '"'+row[1]+'", "' +row[2]+'", "' +row[5]+'", "' +row[7]+'", "'+row[8] +'"'
 			write.write('"'+row[1]+'","' +row[2]+'","' +row[5]+'","' +row[7]+'","'+row[8] +'"\n')
 finally:
@@ -24,6 +26,9 @@ try:
 		if (row[0] == "Impression") or (row[0]=="Copie"):
 	
 			print row
+			nb=nb+int(row[3])
+			couleur=couleur+int(row[4])
 		
 finally:
 	file.close()
+print "Impression nb =" + str(nb) +" Impression couleur = " + str(couleur)
