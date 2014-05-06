47032e77968c387d13121119fa76aa711f3a0a64
root <root@sd-43189.dedibox.fr>
2014-05-06 16:20:04 +0200

    Filtrage type

--- log.py
+++ log.py
@@ -9,10 +9,21 @@ try:
 	reader = csv.reader(file)
 	for row in reader:
 #		print '"'+row[1]+'", "' +row[2]+'", "' +row[5]+'", "' +row[7]+'", "'+row[8] +'"'
-		if (row[5] > "2012-12-29T10:00") and (row[5] < "2013-01-02T10:00"):
+		if (row[5] > "2012-10-29T10:00") and (row[5] < "2013-01-02T10:00"):
 			#	print '"'+row[1]+'", "' +row[2]+'", "' +row[5]+'", "' +row[7]+'", "'+row[8] +'"'
 			write.write('"'+row[1]+'","' +row[2]+'","' +row[5]+'","' +row[7]+'","'+row[8] +'"\n')
 finally:
 
 	file.close()
 	write.close()
+
+file = open("test.csv")
+try:
+	reader = csv.reader(file)
+	for row in reader:
+		if (row[0] == "Impression") or (row[0]=="Copie"):
+	
+			print row
+		
+finally:
+	file.close()
