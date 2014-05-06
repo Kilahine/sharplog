d3bd7123dbccfa3637a86ec717ad7bf895d7e2ec
root <root@sd-43189.dedibox.fr>
2014-05-06 14:57:09 +0200

    Ajout de la condition de date OK

--- log.py
+++ log.py
@@ -8,8 +8,9 @@ file = open(fname, "r")
 try:
 	reader = csv.reader(file)
 	for row in reader:
-		print '"'+row[1]+'", "' +row[2]+'", "' +row[5]+'", "' +row[7]+'", "'+row[8] +'"'
-		
+#		print '"'+row[1]+'", "' +row[2]+'", "' +row[5]+'", "' +row[7]+'", "'+row[8] +'"'
+		if (row[5] > "2012-12-29T10:00") and (row[5] < "2013-01-02T10:00"):
+				print '"'+row[1]+'", "' +row[2]+'", "' +row[5]+'", "' +row[7]+'", "'+row[8] +'"'
 finally:
 
 	file.close()
